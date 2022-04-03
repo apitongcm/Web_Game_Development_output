9e7fb9d238665aa87e93af5cadc7c32f303756a2
apitongcm <apitongcm@gmail.com>
2022-04-03 08:35:56 -0700

    first output for js game dev path. creating animation using canvas

---
+++ game_loop/index.js
@@ -0,0 +1,40 @@
+//always use window.onload for web game development so that it will load all the elements inside before letting the user access the game
+window.onload = function() {
+
+    //states all variables used in the website 
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var x = 0; 
+    var y = 100; 
+
+    //draw the object in canvas 
+    function draw()
+    {
+        //clear canvas
+        context.clearRect(0,0,600,400);
+
+        //adding font to the canvas
+        context.font = '52px Arial';
+        context.fillStyle = 'grey';
+        context.fillText("Hello There!", 150, 100);
+        
+
+        //draw our object in the canvas
+        context.beginPath();
+        context.rect(x,y,100,100);
+        context.fillStyle = "red";
+        context.fill();
+
+        //
+        x += 10 
+        if (x >= 600)
+        {
+            x= -100;
+        }
+        
+
+    }
+    setInterval(draw,100);
+
+
+}
